65276798dd5eb64f24cc2559584bbf3acb2c07f5
anjali790 <anjaliruby790@gmail.com>
2022-11-24 10:12:57 +0530

    movie app

--- Movie-App/app.js
+++ Movie-App/app.js
@@ -1,19 +1,18 @@
-let input = document.querySelector(".input");
-let btn = document.querySelector(".btn");
-let display = document.querySelector(".display");
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const display = document.querySelector(".display")
+const readBtn = document.querySelector(".readbtn");
 
 
 btn.addEventListener("click", function () {
     display.innerHTML = "";
 
     fetch(`https://api.tvmaze.com/search/shows?q=${input.value}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-        for (let i = 0; i < data.length; i++) {
-            let show = data[i]["show"]
-            var html = `<div class ="displayContainer">
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                let show = data[i]["show"]
+                var html = `<div class ="displayContainer">
             <div class="image"><img src="${show["image"]["original"]}" alt=""></div>
             <h2>${show["name"]}</h2>
             <p>${show["premiered"]}</p>
@@ -21,7 +20,7 @@ btn.addEventListener("click", function () {
             <a href="${show["url"]}"><button class = "readBtn">Show Details</button></a> 
             </div>`
 
-            display.innerHTML += html;
-        }
-    });
+                display.innerHTML += html;
+            }
+        });
 });
